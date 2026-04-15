268d5eb7050d21fb9a604cd7568c4209ff47e611
nicolassilvavvh-dev <nicolassilva@liceovvh.cl>
2026-04-15 08:10:41 -0400

    sasa

---
+++ 01_introduccion/1_Hola_mundo/static/js/script.js
@@ -0,0 +1,13 @@
+// Prueba de conexion con js
+console.log("Conexion exitosa con js externo...");
+
+// 1) Hola mundo con variable y const.
+const saludo = "Hola a todos, ";
+let destinatario = "estudiantes";
+// Impresión de saludo concatenando con (+)
+console.log(saludo + "para los " + destinatario);
+
+//2) Saludar a una persona ingresada por teclado
+let nombre = prompt("Ingrese su nombre: ");
+//introducir un nombre ingresado con un prompt
+console.log("Hola, " + nombre);
---
+++ 02_tipos_de_datos/1.-Tipo_texto/static/js/script.js
@@ -0,0 +1,11 @@
+console.log("conexion con js correcta...");
+/*
+Dato: Tipo texto(string)
+Concatenacion de texto con el signo +
+Podemos unir texto y variables
+*/
+const nombre = "Nicolas";
+const apellido = "Perinetti";
+//Unimos ambas constantes con un texto extra.
+console.log("Hola, mi nombre es: " + nombre + " " + apellido)
+
