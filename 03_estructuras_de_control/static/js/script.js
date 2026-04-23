6a8a4ee7a8e7f01d7cba941dbf3133aaac0fae8a
nicolassilvavvh-dev <nicolassilva@liceovvh.cl>
2026-04-23 12:57:04 -0400

    sas

--- 03_estructuras_de_control/static/js/script.js
+++ 03_estructuras_de_control/static/js/script.js
@@ -1,66 +1,76 @@
-console.log("Conexion correcta con js...");
+console.log("conexion con correcta con Js...");
 
-// tipos de datos numricos
-// let --> Palabra clave para crear una variable
-//1.- Declaracion de numeros (Creacion de variables numericas)
-let edad = 31;
-let temperatura = -5;
-let pi = 3.1416;
-let descuento = 0.25;
+/*
+=======================
+1. ¿Qué es una condición en JS?
+=======================
+Una condicion non permite tomar decisiones en el
+codigo separando dos caminos el (if) y el no(else).
 
-//.-Operaciones basicas
+Estructura basica:( Sintaxis --> Reglas del lenguaje de programacion)
 
-let suma = 10 + 5; //15
-let resta = 10- 5; //5
-let multiplicacion = 10 * 5 //50;
-let divicion = 25 / 3;
-let modulo = 10 % 3; //--> Resto de la division %
-let potencia = 2 ** 3; //8
-console.log(`sumar:${suma}, restar:${resta}, multiplicar:${multiplicacion}
-dividir:${divicion}, resto(%):${modulo}, potencia.${potencia}.`);
+if (condicion) {
+    //codigo que se ejecutan si la condicion es verdadera
+} else {
+    //codigo que se ejecutan si la condicion es falsa.
+}
+*/
 
-//3.- incremento y decremento (sumo(++) o resta(--) 1)
-let number = 10;
-number++; //11
-number; //9
+//Ejemplo 1: (numerico)
+let edad = 18;
 
-//4.- numeros con decimales
-let promedio = 6.5;
-let altura = 1.67;
-let peso = 90.3;
+if (edad == 20) { // Condicion si
+    console.log("Eres mayor de edad")
+}
 
-//5.- numeros grandes y notacion cientifica
-let poblacion = 1e6; //1.000.000
-console.log(`numero grande (1e6): ${poblacion}`);
-let numero = 5e-3; //0.005
-console.log(`numero pequeño (5e-3): ${numero}`)
+//Ejemplo 2. Dos caminos posibles
+let temperatura = 21;
 
-//6.- operacion combinadas
-//calcula de precios
-let preioProducto = 1000;
-let iva = 0.19;
-let total = preioProducto + (preioProducto + iva);
-console.log(`el valor final del producto es: ${total}`)
+if (temperatura > 20) { // Condicion Si
+    console.log("Hace calor");
+} else { // Condicion NO
+    console.log("Hace frio")
+}
 
-//calculo de valor de hora trabajando
-let horas = 40;
-let valorHora = 13400;
-let sueldo = horas * valorHora
-console.log(`el sueldo del producto es:${sueldo}`)
+//ejemplo 3: IF - ELSE IF - ELSE (Multiples condiciones)
 
-//redondea de numeros
-console.log("redondeando 4.6: " + Math.round(4.6)) // 5
-console.log("redondeando 4.6: " + Math.floor(4.6)) // 4
+let nota = 5.5;
 
-//8.- Numeros aleatorios
-let aleatorio = Math.random(); // entre 0 y 1
-let dado = Math.floor(Math.random()*6)+1;
-console.log(`Tirar dados; ${dado}`);
+if (nota >= 6.0) {
+    console.log("excelente");
+} else if (nota >= 4.0) {
+    console.log("aprobado")
+} else {
+    console.log("Reprobado estudia mas!")
+}
 
-// 9.- Comparaciones numericas
-let a = 10;
-let b = 5;
-console.log(a > b); //true
-console.log(b > a); //False
-console.log(a === 10); //true (=== estricta igualda)
-console.log( b === 3 ); //False
+//Ejemplo 4: Condiciones con STRING
+let nombre = "Nicolas";
+
+//Comparacion exacta (===)
+if (nombre === "Nicolas"){
+    console.log("Hola, " + nombre)
+} else {
+    console.log("tu no eres Nicolas")
+}
+
+/*
+OPERADORES DE COMPARACION
+
+> mayor que
+< menor que
+>= mayor o igual
+<= menor o iguala
+=== estricta igualdad
+!== distinto estricto
+== igualdad
+!= distinto
+*/
+//Ejemplo de distinto
+let num = 10;
+let num2 = 5
+if (num !== num2){ //Comparacion con distinto
+    console.log(`El numero: ${num} es distinto que ${num2}`)
+} else{
+    console.log("Son iguales")
+}
